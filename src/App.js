4aa2d916872933761a1d91e7593420844b2dc0ac
LARA CONVERSO <laraconverso@gmail.com>
2021-03-28 23:38:56 -0300

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ function App() {
   return (
     <div className="App">
       <h1>ViaBye</h1>
+      <nav className="navBar">
+        <ul>
+          <li>
+            <a href='https://'>Inicio</a>
+          </li>
+          <li>
+            <a href='https://'>Comprar</a>
+          </li>
+          <li>
+            <a href='https://'>Vender</a>
+          </li>
+          <li>
+            <a href='https://'>FAQ</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
