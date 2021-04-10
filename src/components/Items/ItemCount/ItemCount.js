a7b5c955fd515481688759ee4b652f456f3cb163
LARA CONVERSO <laraconverso@gmail.com>
2021-04-10 15:51:27 -0300

    Correccion Contador con botón (addToCart)

--- src/components/Items/ItemCount/ItemCount.js
+++ src/components/Items/ItemCount/ItemCount.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './ItemCount.css';
 import Button from '@material-ui/core/Button';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+
 
 
 class ItemCount extends Component {
@@ -8,6 +10,8 @@ class ItemCount extends Component {
       super();
       this.state = {
         count: 0,
+        stock: 15,
+        min:0,
       };
     }
 
@@ -22,20 +26,33 @@ class ItemCount extends Component {
             this.setState({ count: this.state.count - 1 });
           }
     };
+
+    addToCart = () => {
+      if(this.state.count <1){
+        alert(`No se agrega item por falta de cantidad.`);
+      } else{
+        console.log(
+          `Agregaste ${this.state.count} items a tu carrito` 
+        );
+      }
+    }
   
 
     render() {
       return (
         <div className="Contador-container">
           <div></div>
-            <p>{this.state.count}</p>
+            <p className='cantidad'>{this.state.count}</p>
             <div className='Buttons-container'>
+            <Button className="less" variant="outlined" color="secondary"onClick={this.handleDecrement} size='small'>
+                -
+              </Button>
               <Button  className="plus" variant="outlined" color="secondary" onClick={this.handleIncrement} size='small'>
                 +
               </Button>
-              <Button className="less" variant="outlined" color="secondary"onClick={this.handleDecrement} size='small'>
-                -
-              </Button>
+            </div>
+            <div>
+              <Button className="add" variant="outlined" onClick={this.addToCart}> <AddShoppingCartIcon/> Agregar </Button> 
             </div>
         </div>
 
